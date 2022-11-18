3947d1df134499bd879e4365ccedc164ba95f3d7
Artur Yasnov <65731060+ArturYasnov@users.noreply.github.com>
2022-11-18 10:50:20 +0100

    reformatting code

--- src/retraining.py
+++ src/retraining.py
@@ -1,65 +1,38 @@
 import os
-import gc
-import json
 import random
-import numpy as np
-import pandas as pd
-import tifffile as tiff
-import cv2 as cv
-import albumentations as albu
 from os import path
 
-import pathlib, sys, os, random, time
-import numba, gc, cv2
-import rasterio
-from rasterio.windows import Window
-
-from tqdm.notebook import tqdm
-from PIL import Image
-from sklearn.model_selection import train_test_split
-
-import torch
-import torch.nn as nn
-import torch.utils.data as D
-from torch.utils.data import DataLoader, Dataset
-import torch.nn.functional as F
+import cv2 as cv
+import numpy as np
 import segmentation_models_pytorch as smp
-
-from albumentations import (
-    Compose, OneOf, Normalize, HorizontalFlip, VerticalFlip, RandomRotate90, Transpose, ShiftScaleRotate, IAAAdditiveGaussianNoise, IAAPerspective,
-    CLAHE, RandomBrightness, RandomGamma, IAASharpen, Blur, MotionBlur, RandomContrast, HueSaturationValue, VerticalFlip,
-    RandomRotate90, OneOf, Resize, Rotate, RandomBrightnessContrast, Lambda
-    )
-from albumentations.pytorch import ToTensorV2, ToTensor
+import torch
+import torch.utils.data as D
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
-
-TRAIN_DATA_DIR = path.realpath(path.curdir)+'/Data/256x256/'
-TEST_DATA_DIR = path.realpath(path.curdir)+'/Data/'
-MODEL_SAVE_DIR = path.realpath(path.curdir)+"/models/"
+TRAIN_DATA_DIR = path.realpath(path.curdir) + '/Data/256x256/'
+TEST_DATA_DIR = path.realpath(path.curdir) + '/Data/'
+MODEL_SAVE_DIR = path.realpath(path.curdir) + "/models/"
 TILE_SIZE = 256
 REDUCE_RATE = 4
 SEED = 42
 BATCH_SIZE = 16
 NUM_EPOCHS = 20
 
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
-torch.cuda.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
-
 
 train_img_paths, train_mask_paths = [], []
 for dt in ['train/', 'masks/']:
-    for item in os.listdir(TRAIN_DATA_DIR+dt):
-        if dt=='train/':
-            train_img_paths.append(TRAIN_DATA_DIR+'train/'+item)
-        elif dt=='masks/':
-            train_mask_paths.append(TRAIN_DATA_DIR+'masks/'+item)  
-            
+    for item in os.listdir(TRAIN_DATA_DIR + dt):
+        if dt == 'train/':
+            train_img_paths.append(TRAIN_DATA_DIR + 'train/' + item)
+        elif dt == 'masks/':
+            train_mask_paths.append(TRAIN_DATA_DIR + 'masks/' + item)
+
 paths = list(zip(train_img_paths, train_mask_paths))
 
 
@@ -70,7 +43,7 @@ class TrainDataset(Dataset):
     def __init__(self, paths, transform=None):
         self.paths = paths
         self.transform = transform
-        
+
     def __len__(self):
         return len(self.paths)
 
@@ -79,16 +52,17 @@ class TrainDataset(Dataset):
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         mask = cv.imread(self.paths[i][1], 0)  # 0=cv2.IMREAD_GRAYSCALE, np.array of 0 and 1. (256,256)
         if self.transform:
-            augmented = self.transform(image=image, mask=mask) # image np(256,256,3) -> tensor(3,256,256)
+            augmented = self.transform(image=image, mask=mask)  # image np(256,256,3) -> tensor(3,256,256)
             image, mask = augmented['image'], augmented['mask']
-            mask = mask.unsqueeze(0) # (256,256) -> (1, 256, 256)
+            mask = mask.unsqueeze(0)  # (256,256) -> (1, 256, 256)
         return image, mask
-    
+
+
 class TestDataset(Dataset):
     def __init__(self, paths, transform=None):
         self.paths = paths
         self.transform = transform
-        
+
     def __len__(self):
         return len(self.paths)
 
@@ -105,61 +79,60 @@ class TestDataset(Dataset):
 # Transforms
 # ====================================================
 def get_transforms(*, data):
-    
     if data == 'train':
-        return Compose([
-            HorizontalFlip(p=0.5),
-            VerticalFlip(p=0.5),
-            RandomRotate90(p=0.5),
-            Transpose(p=0.5),
+        return A.Compose([
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.RandomRotate90(p=0.5),
+            A.Transpose(p=0.5),
 
-            ShiftScaleRotate(scale_limit=0.2, rotate_limit=0, shift_limit=0.2, p=0.2, border_mode=0),
+            A.ShiftScaleRotate(scale_limit=0.2, rotate_limit=0, shift_limit=0.2, p=0.2, border_mode=0),
 
-            IAAAdditiveGaussianNoise(p=0.2),
-            IAAPerspective(p=0.5),
+            A.IAAAdditiveGaussianNoise(p=0.2),
+            A.IAAPerspective(p=0.5),
 
-            OneOf([
-                    CLAHE(p=1),
-                    RandomBrightness(p=1),
-                    RandomGamma(p=1),
-                ], p=0.9,
-            ),
-
-            OneOf([
-                IAASharpen(p=1),
-                Blur(blur_limit=3, p=1),
-                MotionBlur(blur_limit=3, p=1),
+            A.OneOf([
+                A.CLAHE(p=1),
+                A.RandomBrightness(p=1),
+                A.RandomGamma(p=1),
             ], p=0.9,
             ),
 
-            OneOf([   
-                RandomContrast(p=1),
-                HueSaturationValue(p=1),],  p=0.9,
+            A.OneOf([
+                A.IAASharpen(p=1),
+                A.Blur(blur_limit=3, p=1),
+                A.MotionBlur(blur_limit=3, p=1),
+            ], p=0.9,
             ),
-            Compose([
-                VerticalFlip(p=0.5),              
-                RandomRotate90(p=0.5)]
+
+            A.OneOf([
+                A.RandomContrast(p=1),
+                A.HueSaturationValue(p=1), ], p=0.9,
             ),
-            Normalize(
+            A.Compose([
+                A.VerticalFlip(p=0.5),
+                A.RandomRotate90(p=0.5)]
+            ),
+            A.Normalize(
                 mean=[0.485, 0.456, 0.406],
                 std=[0.229, 0.224, 0.225],
             ),
-            ToTensorV2(),  #ToTensor(num_classes=2),
-        ])    
+            A.ToTensorV2(),  # ToTensor(num_classes=2),
+        ])
     elif data == 'valid':
-        return Compose([
-            Resize(256,256),
-            Normalize(
+        return A.Compose([
+            A.Resize(256, 256),
+            A.Normalize(
                 mean=[0.485, 0.456, 0.406],
                 std=[0.229, 0.224, 0.225],
             ),
-            ToTensorV2(),
+            A.ToTensorV2(),
         ])
 
 
 train_paths, val_paths = train_test_split(paths, test_size=0.2, random_state=SEED, shuffle=True)
-train_dataset = TrainDataset(train_paths, transform=get_transforms(data='train'),)
-valid_dataset = TrainDataset(val_paths, transform=get_transforms(data='valid'),)
+train_dataset = TrainDataset(train_paths, transform=get_transforms(data='train'), )
+valid_dataset = TrainDataset(val_paths, transform=get_transforms(data='valid'), )
 
 train_loader = D.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, pin_memory=True)
 valid_loader = D.DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, pin_memory=True)
@@ -167,8 +140,7 @@ valid_loader = D.DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False,
 # ====================================================
 # Modeling
 # ====================================================
-model = smp.Unet(encoder_name='se_resnext50_32x4d', encoder_weights='imagenet', activation='sigmoid')
-# model = smp.FPN(encoder,encoder_weights=encoder_weights)
+model = smp.DeepLabV3Plus(encoder_name='se_resnext50_32x4d', encoder_weights='imagenet', activation='sigmoid')
 
 loss = smp.utils.losses.DiceLoss()
 metrics = [
@@ -177,20 +149,19 @@ metrics = [
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, amsgrad=False)
 
-
 train_epoch = smp.utils.train.TrainEpoch(
-    model, 
-    loss=loss, 
-    metrics=metrics, 
+    model,
+    loss=loss,
+    metrics=metrics,
     optimizer=optimizer,
     device=device,
     verbose=True,
 )
 
 valid_epoch = smp.utils.train.ValidEpoch(
-    model, 
-    loss=loss, 
-    metrics=metrics, 
+    model,
+    loss=loss,
+    metrics=metrics,
     device=device,
     verbose=True,
 )
@@ -204,7 +175,7 @@ for i in range(0, n_epochs):
     print('\nEpoch: {}'.format(i))
     train_logs = train_epoch.run(train_loader)
     valid_logs = valid_epoch.run(valid_loader)
-    
+
     train_losses.append(train_logs['dice_loss'])
     val_losses.append(valid_logs['dice_loss'])
     train_scores.append(train_logs['iou_score'])
@@ -214,9 +185,3 @@ for i in range(0, n_epochs):
         best_loss = valid_logs['dice_loss']
         torch.save(model, os.path.join(MODEL_SAVE_DIR, 'model_retrained.pth'))
         print('Model saved!')
-
-
-
-
-
-
